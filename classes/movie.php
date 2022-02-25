e15180e97bb4ebb54e8b5d64e79a9e6133e10334
DanieleFic <reb_ficus22@hotmail.it>
2022-02-25 15:15:10 +0100

    first commit

---
+++ classes/movie.php
@@ -0,0 +1,20 @@
+<?php
+    class Film{
+        private $titolo;
+        private $genere;
+        private $datauscita;
+
+
+        public function __construct($_titolo,$_genere,$_datauscita){
+            $this->titolo = $_titolo;
+            $this->genere = $_genere;
+            $this->datauscita = $_datauscita;
+        }
+
+        public function getfilm(){
+
+            return "{$this->titolo} {$this->genere} {$this->datauscita}";
+        }
+        
+    }
+
---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+
+    require_once __DIR__. "./classes/movie.php";
+
+    $filmArr = [
+        new Film("The Batman","Cinecomic","3 marzo 2022"),
+        new Film("Joker","Cinecomic","3 ottobre 2019")
+    ];
+    
+
+?>
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Es oop</title>
+    </head>
+    <body>
+            <ul>
+                <?php
+                foreach($filmArr as $elemento){
+                    echo "<li>{$elemento->getfilm()}</li>";
+                }?>
+            </ul>
+    </body>
+    </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
